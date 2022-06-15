c7781dfc7a52100dc64b6368b0c38f5158a5f5ac
DinushaSa <73377334+DinushaSa@users.noreply.github.com>
2022-06-15 17:26:38 +0530

    Update auth-service.service.ts

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -12,9 +12,6 @@ export class AuthServiceService {
  public datakey:any=dataKey;
   constructor(public http: HttpClient) { }
   
-
-
-
   getLogin(username:string, password:string){
     return new Promise ((resolve, reject) => {
     let headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8','Authorization': 'Basic '+btoa(username + ':'  +password)});
